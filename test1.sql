ef078fd7ca380987378eb1483ce52b6a8043c216
jorgevarela98 <jormivaroli98@hotmail.com>
2021-04-16 13:45:35 -0600

    bd1 test github

---
+++ test1.sql
@@ -0,0 +1,10 @@
+/*
+This is a comment lmfao
+*/
+-- ----------------
+-- COmmmeeennnnttttt
+-- Motherfucker
+-- ----------------
+
+/* Se crea una base de datos llamada Menú*/
+CREATE DATABASE Menu;
